4ef638445dacc933bb8b0c89404e452bb1ef22e7
Pep Martinez <pep.martinez@gmail.com>
2017-06-04 18:39:26 +0200

    use keuss v1.0.0

--- config.js
+++ config.js
@@ -1,45 +1,61 @@
 
-var stats_redis = require ('keuss/stats/redis');
-var stats_mem =   require ('keuss/stats/mem');
+var stats_redis = require('keuss/stats/redis');
+var stats_mem = require('keuss/stats/mem');
 
-var signal_redis_pubsub = require ('keuss/signal/redis-pubsub');
-var signal_local =        require ('keuss/signal/local');
+var signal_redis_pubsub = require('keuss/signal/redis-pubsub');
+var signal_local = require('keuss/signal/local');
 
-var winston = require ('winston');
+var winston = require('winston');
 
 var logger = new (winston.Logger)({
   transports: [
-    new (winston.transports.Console)({level: 'info'})
+    new (winston.transports.Console)({ level: 'info' })
   ]
 });
 
 var config = {
   logger: logger,
   queues: {
-    logger: logger,
-    pollInterval: 17000,
-    stats: {
-      provider: stats_redis,
-      opts: {}
-    },
-    signaller: {
-      provider: signal_redis_pubsub,
-      opts: {}
-    }
+    logger: logger
   },
   backends: [
     {
       factory: 'mongo',
-//      disable: true,
+      //      disable: true,
       config: {
-        url: 'mongodb://localhost:27017/keuss'
+        url: 'mongodb://localhost:27017/keuss',
+        pollInterval: 17000,
+        stats: {
+          provider: new stats_redis(),
+        },
+        signaller: {
+          provider: new signal_redis_pubsub()
+        }
       }
     },
     {
       factory: 'redis-list',
+      config: {
+        pollInterval: 17000,
+        stats: {
+          provider: new stats_redis(),
+        },
+        signaller: {
+          provider: new signal_redis_pubsub()
+        }
+      }
     },
     {
       factory: 'redis-oq',
+      config: {
+        pollInterval: 17000,
+        stats: {
+          provider: new stats_redis(),
+        },
+        signaller: {
+          provider: new signal_redis_pubsub()
+        }
+      }
     }
   ]
 };
