e37f90d86591649f8c624597ce195e421b1bf3be
Pep Martinez <pep.martinez@gmail.com>
2023-01-01 17:54:38 +0100

    added pub/sub examples on stomp-cl

---
+++ stomp-cl/stream-sub.js
@@ -0,0 +1,49 @@
+var stompit = require('stompit');
+
+
+var connectOptions = {
+  'host': 'localhost',
+  'port': 61613,
+  'connectHeaders':{
+    'host': '/',
+    'login': 'username',
+    'passcode': 'password',
+    'heart-beat': '5000,5000'
+  }
+};
+
+stompit.connect(connectOptions, function(error, client) {
+  if (error) {
+    console.log('connect error ' + error.message);
+    return;
+  }
+
+  var subscribeHeaders = {
+    destination: '/q/streams/stomp_test_or_else',
+    'x-parallel': 7,
+    'x-wsize': 3,
+    'x-ks-group': 'r',
+    ack: 'client-individual'
+//    ack: 'auto'
+  };
+
+  client.subscribe(subscribeHeaders, function(error, message) {
+    if (error) {
+      console.log('subscribe error ' + error.message);
+      return;
+    }
+
+    message.readString('utf-8', function(error, body) {
+      if (error) {
+        console.log('read message error ' + error.message);
+        return;
+      }
+
+      console.log(new Date() + ': received message:' + body);
+      client.ack(message);
+
+//      client.nack(message);
+//      client.disconnect();
+    });
+  });
+});
